df9b6a8c581605106d668c64a1d4f25f4e2d8bcb
Jatani Ali <mrcortesyali@gmail.co>
2025-12-04 12:30:34 +0300

    cors issue

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,8 @@ const corsOptions = {
   origin: [
     'http://localhost:5173', 
     'http://127.0.0.1:5173',
-    'https://hms-frontend-eov9.onrender.com'
+    'https://hms-frontend-eov9.onrender.com',
+    'https://hms-mmyd.onrender.com'
   ],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'],
